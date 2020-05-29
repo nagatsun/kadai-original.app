b55dbeb3d4b5b97efddef0a92c3875ae5a32773e
taka.aca <taka.aca@techacademy.jp>
2020-05-29 13:02:18 +0000

    follow, unfollow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,7 @@ class ApplicationController < ActionController::Base
   
   def counts(user)
     @count_tecposts = user.tecposts.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
   end
 end
